ef3f58007bf9613dac95fa0e4ba7c70f8289cfe7
nadiajurkovich <nmj2120@columbia.edu>
2021-03-30 14:36:37 -0400

    Added to amniguity.py

--- ambiguity.py
+++ ambiguity.py
@@ -1,7 +1,7 @@
 # Code adapted from https://www.nltk.org/_modules/nltk/wsd.html
 
 from nltk.corpus import wordnet
-
+from nltk.corpus import stopwords
 
 def lesk_ambiguity_score(context_sentence, ambiguous_word, pos=None, synsets=None):
     context = set(context_sentence)
@@ -15,8 +15,39 @@ def lesk_ambiguity_score(context_sentence, ambiguous_word, pos=None, synsets=Non
         return None
 
     lesk_scores = [len(context.intersection(ss.definition().split())) for ss in synsets]
-    second_highest, highest = sorted(lesk_scores)[-1:]
+    second_highest, highest = sorted(lesk_scores)[-2:]
 
     if second_highest == 0:
         return 0.
     return second_highest / highest
+
+def main():
+    sw = stopwords.words("english")
+
+    filepath = 'data/dev.tsv'
+    with open(filepath) as fp:
+       line = fp.readline()
+       while line:
+           cnt = 0
+           index = 0
+           for i in line:
+               index += 1
+               if i == ",":
+                   cnt += 1
+               if cnt == 3:
+                   sentence = line[index:]
+                   print(sentence)
+
+                   for word in sentence.split():
+                       if word in sw: #if the word is a stopword
+                           print(0)
+                       else:
+                           print(lesk_ambiguity_score(sentence, word))
+
+
+                   cnt = -1000 #reset count so nothing will be printed for the same line again
+
+           line = fp.readline() #next line
+
+if __name__ == "__main__":
+    main()
